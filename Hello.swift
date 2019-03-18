b8efd7aa5b05266751222314698799918ada7638
ohmygymofficial <contact@ohmygym.fr>
2019-03-18 10:23:23 +0100

    Base code created to asking question to JOE, choice 1 2 3 will be developped later

---
+++ Hello.swift
@@ -0,0 +1,8 @@
+
+// Quand tu met environ 30 minutes à trouver une erreur dans 3 lignes de code débutant ^^
+// Majuscule à "If" ne donne pas pareil que "if"
+print("Comment vous appelez vous ?")
+
+if let name = readLine() {
+    print("Bonjour \(name)!")
+}
